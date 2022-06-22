99bf396b40a586edb204172f5b056cc4fd7def14
KarinaKushnir <karina.kushnir97@gmail.com>
2022-06-23 01:28:27 +0300

    add redux toolkit

--- weather/src/pages/home/Home.tsx
+++ weather/src/pages/home/Home.tsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+import { useCustomDispatch, useCustomSelector } from "../../hooks/store";
+import { selectCurrentWeatherData } from "../../store/selectors/selector";
+import { fetchCurrentWeather } from "../../store/thunks/fetchCurrentWeather";
 import { AllDays } from "./components/all-days/AllDays";
 import { DayInfo } from "./components/day-info/DayInfo";
 import { Day } from "./components/day/Day";
@@ -6,10 +10,15 @@ import styles from "./Home.module.scss";
 type HomeProps = {};
 
 export const Home: React.FC<HomeProps> = () => {
+  const dispatch = useCustomDispatch();
+  const { weather } = useCustomSelector(selectCurrentWeatherData);
+  useEffect(() => {
+    dispatch(fetchCurrentWeather("paris"));
+  }, []);
   return (
     <div className={styles.home}>
       <div className={styles.day__container}>
-        <Day />
+        <Day weather={weather} />
         <DayInfo />
       </div>
       <AllDays></AllDays>
--- weather/src/pages/home/components/day/Day.tsx
+++ weather/src/pages/home/components/day/Day.tsx
@@ -1,14 +1,19 @@
 import { GlobalSvgSelector } from "../../../../assets/icons/global/GlobalSvgSelector";
+import { Weather } from "../../../../store/types/types";
 import styles from "./Day.module.scss";
 
-type DayProps = {};
+type DayProps = {
+  weather: Weather;
+};
 
-export const Day: React.FC<DayProps> = () => {
+export const Day: React.FC<DayProps> = ({ weather }) => {
   return (
     <div className={styles.this__day}>
       <div className={styles.top__block}>
         <div className={styles.top__block_wrapper}>
-          <div className={styles.this__temp}>20°</div>
+          <div className={styles.this__temp}>
+            {Math.floor(weather.main.temp)}
+          </div>
           <div className={styles.this__day_name}>Сегодня</div>
         </div>
         <GlobalSvgSelector id="sun" />
